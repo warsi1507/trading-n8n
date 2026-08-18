084f6f1fed3c945319dce548a38e167d6d92f283
warsi1507 <mdsamarwarsi@outlook.com>
2026-08-18 23:56:25 +0530

    fix(executor): resolve tsconfig moduleResolution deprecation warning

--- app/executor/dist/index.js
+++ app/executor/dist/index.js
@@ -1,11 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-const db_1 = require("@trading-n8n/db");
-dotenv_1.default.config();
+import dotenv from "dotenv";
+import { connectDB } from "@trading-n8n/db";
+dotenv.config();
 async function startExecutor() {
     console.log("Starting Execution Engine...");
     const MONGO_URI = process.env.MONGO_URI;
@@ -13,7 +8,7 @@ async function startExecutor() {
         console.error("MONGO_URI is missing in environment.");
         process.exit(1);
     }
-    await (0, db_1.connectDB)(MONGO_URI);
+    await connectDB(MONGO_URI);
     console.log("Connected to Database.");
     console.log("Executor is running and waiting for workflows...");
     // TODO: Setup polling, cron jobs, or message queues to trigger executions
